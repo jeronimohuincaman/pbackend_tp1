495e58155d606182df23c87ce634da6331b5d9fe
LucasDL23 <lucasmartindiluca@gmail.com>
2024-04-09 16:34:27 -0300

    More changes

---
+++ src/addPerson.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+const addButton = document.getElementById('addButton');
+const formulario = document.getElementById('formulario');
+
+addButton.addEventListener('click', function () {
+    formulario.style.display = 'block;';
+});
+});
